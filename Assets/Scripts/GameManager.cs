93e40df7e52ea304dcbad8c320f32633dcb8183b
Brandon Duong <brandon.duong@outlook.com>
2020-08-10 07:05:03 -0400

    Can now trigger player game overs.
    Can now trigger player game overs.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameWin = true;
+    private bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void EndGame()
+    {
+        if (!gameOver)
+        {
+            gameOver = true;
+
+            // If game over, restart level
+            Restart();
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/GameOverTriggers.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameOverTriggers : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void Update()
+    {
+        // If player falls below camera sight (down a hole), game over
+        if (gameObject.GetComponent<Rigidbody2D>().position.y < -8.55f)
+        {
+            gameManager.EndGame();
+        }
+    }
+
+    // Called whenever current objects collides with something (Needs a rigid boy + collider)
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
