59d007e969c5f195e2e1cdb4f77a1c36b692fb4f
Brandon Duong <brandon.duong@outlook.com>
2020-08-16 05:36:53 -0400

    Created tutorial popups!
    Created tutorial popups!

---
+++ Assets/AppearWhenClose.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearWhenClose : MonoBehaviour
+{
+    private Animator animator;
+    public GameObject player;
+    private RectTransform rect;
+    public float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rect = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<Rigidbody2D>().position.x <= rect.position.x + range
+            && player.GetComponent<Rigidbody2D>().position.x >= rect.position.x - range)
+        {
+            animator.SetBool("visible", true);
+        }
+        else
+        {
+            animator.SetBool("visible", false);
+        }
+    }
+}
