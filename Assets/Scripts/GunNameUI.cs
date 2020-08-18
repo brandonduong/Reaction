17a9c3cb8d873de056019b75f10166614c4f378a
Brandon Duong <brandon.duong@outlook.com>
2020-08-18 05:43:24 -0400

    Added a display for the current gun's name to show up!
    Added a display for the current gun's name to show up!

--- Assets/Scripts/GunNameUI.cs
+++ Assets/Scripts/GunNameUI.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AmmoUI : MonoBehaviour
+public class GunNameUI : MonoBehaviour
 {
-    private Text ammoText;
+    private Text gunNameText;
     private GunManager gunManager;
 
     private void Start()
     {
-        ammoText = GetComponent<Text>();
+        gunNameText = GetComponent<Text>();
         gunManager = FindObjectOfType<GunManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ammoText.text = gunManager.currentGun.CurrentAmmo.ToString() + "/" + gunManager.currentGun.MaxAmmo.ToString();
+        // Get name of current gun
+        gunNameText.text = gunManager.currentGun.GetType().Name;
     }
 }
